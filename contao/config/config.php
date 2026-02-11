b1e51db48264e6e3fb403e223576a88f7c64a5bb
websailing <info@websailing.de>
2026-02-11 13:11:02 +0100

    V 0.9.2

--- contao/config/config.php
+++ contao/config/config.php
@@ -1,5 +1,5 @@
 <?php
 
 // Dummy mapping for legacy visitors module to prevent runtime errors in Contao 5
-$GLOBALS['FE_MOD']['miscellaneous']['visitors'] = \DVC\TemplateSupport\Module\ModuleVisitorsDummy::class;
-
+// Use correct namespace for the dummy module class.
+$GLOBALS['FE_MOD']['miscellaneous']['visitors'] = \Dvc\ContaoTemplateSupportBundle\Module\ModuleVisitorsDummy::class;
